2c78b3d810f9818178180d6c9ead7020ef1f7a6e
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-03-08 12:41:20 -0600

    Estructura de htm y data de test

--- test/data.js
+++ test/data.js
@@ -1,5 +1,34 @@
 // Aqui puedes agregar los datos que necesites para tus pruebas
 // al menos debe tener uno que se llame "data"
 
-export const data = [];
+export const data = [{// 01_Volcán Arenal
+  "id": "01_lugarTuristico",
+  "name": "Volcán Arenal",
+  "location": "Alajuela",
+  "shortDescription": "Impresionante volcán activo con vistas panorámicas.",
+  "description": "El Volcán Arenal es uno de los destinos turísticos más destacados de Costa Rica. Con su cono perfecto y actividad volcánica ocasional, ofrece experiencias inolvidables para los amantes de la naturaleza y la aventura. Explora los senderos cercanos, relájate en aguas termales en las faldas del Volcán y maravíllate con la biodiversidad que rodea este icónico volcán. Observa una gran variedad de flora y fauna entre ellos aves, reptiles y mamíferos en su hábitat natural, mientras disfrutas la satisfacción de contribuir a la conservación de este ecosistema.",
+  "imageUrl": "https://github.com/Maddyrojas/DEV014-Dataverse/blob/main/Imagenes%20Dataverse/01_lugarTuristico.jpg",
+  "tipoTurismo": "turismo de aventura",
+  "gastoPromedio": 150,
+  "facts": {
+    "museo": "Museo de Volcanología",
+    "compras": "Arte Tico",
+    "alimentacion": "Restaurante El Cráter"
+  }
+},
+{//02_Parque Nacional Manuel Antonio
+  "id": "02_lugarTuristico",
+  "name": "Parque Nacional Manuel Antonio",
+  "location": "Puntarenas",
+  "shortDescription": "Reserva natural con playas prístinas y diversidad de vida silvestre.",
+  "description": "Manuel Antonio es un paraíso costero que combina impresionantes playas de arena blanca con exuberantes selvas tropicales. Este parque nacional es hogar de una increíble diversidad de vida silvestre, incluyendo monos capuchinos, perezosos, iguanas, pizotes y aves exóticas. Disfruta de emocionantes caminatas por la selva, relájate en las playas prístinas y observa el atardecer en este destino único. El Parque Nacional Manuel Antonio es un lugar perfecto para quienes buscan una experiencia única que combina aventura, amor por la naturaleza y relajación.",
+  "imageUrl": "https://github.com/Maddyrojas/DEV014-Dataverse/blob/main/Imagenes%20Dataverse/02_lugarTuristico.jpg",
+  "tipoTurismo": "turismo de playa",
+  "gastoPromedio": 120,
+  "facts": {
+    "museo": "undefined",
+    "compras": "Mercado Artesanal Manuel Antonio",
+    "alimentacion": "Restaurante Mariscos del Pacífico"
+  }
+}];
 
