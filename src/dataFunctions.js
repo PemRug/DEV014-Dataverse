1b941710e2a740947657047a2d40da350161073d
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-03-12 21:41:13 -0600

    ajuste de cambios

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -4,10 +4,4 @@ export const example = () => {
   return 'example';
 };
 
-export const filterData = (data,filterBy,value) => {
-  const locationData = document.querySelector("dt[itempromp='location']");
 
-  const filterData = locationData.filter(());
-
-  return [];
-};
--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
-import { example } from './dataFunctions.js';
+
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
-
 const content = document.getElementById('root');
 content.appendChild(renderItems(data));
 
-
-
-const filterProvincia = document.querySelector('select[name="filtrarProvincia"]');
-    filterProvincia.addEventListener('change', () => {
-    console.log(e.target) (e)
-    console.log(filterProvincia.value);
-    example.filterData(data,'location',filterProvincia.value)
-    });
-
-
-    
