789b6a7163000f3f08a95a50a0645c04fa21673c
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-03-14 10:34:00 -0600

    actualizacion de metodo filter

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -4,4 +4,7 @@ export const example = () => {
   return 'example';
 };
 
-
+export const filterData = (data, filterBy, value) => {
+  const newData = data.filter(tour=> tour[filterBy] === value.value);
+  return newData;
+};
--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import { filterData } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
 
 const content = document.getElementById('root');
+const filterProvincia = document.querySelector('select[name="filtrarProvincia"]');
+
 content.appendChild(renderItems(data));
 
-
-
-const filterProvincia = document.querySelector('select[name="filtrarProvincia"]');
 filterProvincia.addEventListener('change', () => {
-    const ul = document.querySelector('ul');
-    ul.remove();
-    content.appendChild(renderItems(filterData(data,filterProvincia)));
+  document.querySelector('ul').remove();
+  content.appendChild(renderItems(filterData(data,'location',filterProvincia)));
 });
     
