72ae38d0923c9bc16b411060d05ad183e117b29e
Madelyn Rojas Moya <mmrojasmoya@gmail.com>
2024-03-15 15:55:26 -0600

    trabajando en el boton

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -5,6 +5,5 @@ export const example = () => {
 };
 
 export const filterData = (data, filterBy, value) => {
-  const newData = data.filter(tour=> tour[filterBy] === value.value);
-  return newData;
+  return data.filter(tour=> tour[filterBy] === value.value);
 };
