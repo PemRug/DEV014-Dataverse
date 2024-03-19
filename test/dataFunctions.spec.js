07587aa05ab596d79a54b97f95f5320ff6704200
PemRug <paolamejia955@gmail.com>
2024-03-19 11:36:03 -0500

    Test de SortOf funcional

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,9 +1,9 @@
-import { filterData} from '../src/dataFunctions.js';
+import { filterData, sortData } from '../src/dataFunctions.js';
 import { data as fakeData } from './data.js';
 
 
 describe('testDataFunctions', () => {
-  const expected = [{// 01_Volcán Arenal
+  const expectedFilter = [{// 01_Volcán Arenal
     "id": "01_lugarTuristico",
     "name": "Volcán Arenal",
     "location": "Alajuela",
@@ -11,7 +11,26 @@ describe('testDataFunctions', () => {
     "tipoTurismo": "turismo de aventura",
     "gastoPromedio": 150,
   },];
+  const expectedSort = [{//02_Parque Nacional Manuel Antonio
+    "id": "02_lugarTuristico",
+    "name": "Parque Nacional Manuel Antonio",
+    "location": "Puntarenas",
+    "shortDescription": "Reserva natural con playas prístinas y diversidad de vida silvestre.",
+    "tipoTurismo": "turismo de playa",
+    "gastoPromedio": 120,
+  },
+  {// 01_Volcán Arenal
+    "id": "01_lugarTuristico",
+    "name": "Volcán Arenal",
+    "location": "Alajuela",
+    "shortDescription": "Impresionante volcán activo con vistas panorámicas.",
+    "tipoTurismo": "turismo de aventura",
+    "gastoPromedio": 150,
+  },]
   it('devuelve `filterData` con array de objetos key:value', () => {
-    expect(filterData(fakeData, 'location', 'Alajuela')).toEqual(expected);
+    expect(filterData(fakeData, 'location', 'Alajuela')).toEqual(expectedFilter);
   });
+  it('devuelve `SortOf` con un array ordenado de objetos key:value', () => {
+    expect(sortData(fakeData, 'name', 'asc')).toEqual(expectedSort);
+  })
 });
